2ad622cec40faf65ff4a15d898b28de02efc960c
Devon Bautista <17506592+synackd@users.noreply.github.com>
2026-08-24 11:03:37 -0600

    chore(reuse): fix copyright
    Signed-off-by: Devon Bautista <17506592+synackd@users.noreply.github.com>

--- examples/profile-requests.sh
+++ examples/profile-requests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2025 OpenCHAMI Contributors
+# SPDX-FileCopyrightText: © 2025 OpenCHAMI a Series of LF Projects, LLC
 # SPDX-License-Identifier: MIT
 #
 # Example commands for requesting boot scripts with profiles.
